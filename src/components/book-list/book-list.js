0b75503cca3390a27fcd8addf2323e2605767120
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-02-18 22:47:36 +0600

    action dispatch

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import BookListItem from '../book-list-item';
+// import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+import { withBookstoreService } from '../hoc';
+import { booksLoaded } from '../../actions';
+import compose from '../../utils';
 import './book-list.css';
 
 class BookList extends Component {
+  componentDidMount() {
+    const { bookstoreService } = this.props;
+    const data = bookstoreService.getBooks();
+    this.props.booksLoaded(data);
+  }
   render() {
     const { books } = this.props;
     return (
@@ -25,4 +34,36 @@ const mapStateToProps = ({ books }) => ({
   books,
 });
 
-export default connect(mapStateToProps)(BookList);
+// ----------------------------- 1 Форма ---------------------------
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     booksLoaded: newBooks => {
+//       dispatch({
+//         type: 'BOOKS_LOADED',
+//         payload: newBooks,
+//       });
+//     },
+//   };
+// };
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     booksLoaded: newBooks => {
+//       dispatch(booksLoaded(newBooks));
+//     },
+//   };
+// };
+
+// const mapDispatchToProps = dispatch => {
+//   return bindActionCreators({ booksLoaded }, dispatch);
+// };
+
+// ----------------------------- 2 Форма ---------------------------
+const mapDispatchToProps = {
+  booksLoaded,
+};
+
+export default compose(
+  withBookstoreService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
