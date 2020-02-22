fe8aa1353792538fe329b8bca9878c2a894c2753
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-02-22 21:28:19 +0600

    connection component to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,24 +2,48 @@ const initialState = {
   books: [],
   loading: true,
   error: null,
+  cartItems: [
+    {
+      id: 1,
+      name: 'Book 1',
+      count: 3,
+      total: 150,
+    },
+    {
+      id: 2,
+      name: 'Book 2',
+      count: 3,
+      total: 60,
+    },
+    {
+      id: 3,
+      name: 'Book 3',
+      count: 3,
+      total: 440,
+    },
+  ],
+  orderTotal: 650,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_BOOKS_REQUEST':
       return {
+        ...state,
         books: [],
         loading: true,
         error: null,
       };
     case 'FETCH_BOOKS_SUCCESS':
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null,
       };
     case 'FETCH_BOOKS_FAILURE':
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload,
