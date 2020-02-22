cc0d80968f5addb3fa5b7ae6be805c4b674299fc
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-02-22 19:13:26 +0600

    mapDispatchToProps  and ownProps

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -12,14 +12,7 @@ import './book-list.css';
 
 class BookList extends Component {
   componentDidMount() {
-    const { bookstoreService, booksLoaded, booksRequested, booksError } = this.props;
-    booksRequested();
-    bookstoreService
-      .getBooks()
-      .then(data => booksLoaded(data))
-      .catch(err => {
-        booksError(err);
-      });
+    this.props.fetchBooks();
   }
   render() {
     const { books, loading, error } = this.props;
@@ -51,10 +44,19 @@ const mapStateToProps = ({ books, loading, error }) => ({
   error,
 });
 
-const mapDispatchToProps = {
-  booksLoaded,
-  booksRequested,
-  booksError,
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const { bookstoreService } = ownProps;
+  return {
+    fetchBooks: () => {
+      dispatch(booksRequested());
+      bookstoreService
+        .getBooks()
+        .then(data => dispatch(booksLoaded(data)))
+        .catch(err => {
+          dispatch(booksError(err));
+        });
+    },
+  };
 };
 
 export default compose(
