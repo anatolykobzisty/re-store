620ef8899752e08f1f18a286c52442e58d9d7179
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-02-23 18:45:14 +0600

    store enhancer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,42 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const stringEnhancer = createStore => (...args) => {
+  const store = createStore(...args);
+  const origignalDispatch = store.dispatch;
+  store.dispatch = action => {
+    if (typeof action === 'string') {
+      return origignalDispatch({ type: action });
+    }
+
+    return origignalDispatch(action);
+  };
+  return store;
+};
+
+const logEnhancer = createStore => (...args) => {
+  const store = createStore(...args);
+  const origignalDispatch = store.dispatch;
+  store.dispatch = action => {
+    console.log(action.type);
+    return origignalDispatch(action);
+  };
+  return store;
+};
+
+// monkey patching
+
+// const origignalDispatch = store.dispatch;
+// store.dispatch = action => {
+//   if (typeof action === 'string') {
+//     return origignalDispatch({ type: action });
+//   }
+
+//   return origignalDispatch(action);
+// };
+
+const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+
+store.dispatch('HELLO_WORLD');
 
 export default store;
