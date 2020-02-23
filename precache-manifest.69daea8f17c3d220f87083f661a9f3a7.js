0326c7b86507538b41e9d6441e0dbc4f97e43cfa
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-02-23 23:18:39 +0600

    Updates

---
+++ precache-manifest.69daea8f17c3d220f87083f661a9f3a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba330b3bf53b24f4f14130d92c00bc2",
+    "url": "/re-store/index.html"
+  },
+  {
+    "revision": "9b69b18a58a2ff8ecee6",
+    "url": "/re-store/static/css/main.b235f277.chunk.css"
+  },
+  {
+    "revision": "e0fdbcca0ba5904e3ad5",
+    "url": "/re-store/static/js/2.b6cd3d2b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/re-store/static/js/2.b6cd3d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b69b18a58a2ff8ecee6",
+    "url": "/re-store/static/js/main.ba2653ba.chunk.js"
+  },
+  {
+    "revision": "04e46cd94b813b324222",
+    "url": "/re-store/static/js/runtime-main.8304c6f7.js"
+  }
+]);
